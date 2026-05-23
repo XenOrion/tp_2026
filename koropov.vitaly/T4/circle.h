d03fac1eea44381264f97fde43f2dc8c7915624c
vitalyasq <sqvitalya@gmail.com>
2026-05-23 14:27:31 +0300

    Загрузил Т4

---
+++ koropov.vitaly/T4/circle.h
@@ -0,0 +1,23 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include "point.h"
+#include "shape.h"
+#include <string>
+
+class Circle : public Shape {
+private:
+	Point center;
+	double radius;
+public:
+	Circle(Point center, double  radius);
+
+	double getArea() const override;
+	Point getCenter() const override;
+	void move(double dx, double dy) override;
+	void scale(double factor) override;
+	std::string getName() const override;
+	Point getMinPoint() const override;
+	Point getMaxPoint() const override;
+}; 
+#endif
---
+++ koropov.vitaly/T4/compositeshape.h
@@ -0,0 +1,26 @@
+#ifndef COMPOSITESHAPE_H
+#define COMPOSITESHAPE_H
+
+#include "point.h"
+#include "shape.h"
+#include <string>
+#include <vector>
+#include <memory>
+
+class CompositeShape : public Shape {
+private:
+	std::vector<std::shared_ptr<Shape>> shapes;
+public:
+	CompositeShape();
+
+	void addShape(std::shared_ptr<Shape> shape);
+
+	double getArea() const override;
+	Point getCenter() const override;
+	void move(double dx, double dy) override;
+	void scale(double factor) override;
+	std::string getName() const override;
+	Point getMinPoint() const override;
+	Point getMaxPoint() const override;
+};
+#endif
